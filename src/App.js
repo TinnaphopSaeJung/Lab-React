08bdbeb978c9c934448819de51a8824dc0f87e71
TinnaphopSaeJung <6210110484@psu.ac.th>
2022-08-15 21:53:44 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <WordCard value={word}/>
     </div>
   );
 }
